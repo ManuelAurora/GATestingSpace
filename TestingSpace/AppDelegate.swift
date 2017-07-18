1d9dd6ad1ad3359fd6e3d3dec6dade36fad407f7
Manuel Aurora <amp.smichrissoft@gmail.com>
2017-07-18 09:46:03 +0300

    Initial Commit

---
+++ TestingSpace/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TestingSpace
+//
+//  Created by Manuel Aurora on 18.07.17.
+//  Copyright © 2017 Manuel Aurora. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TestingSpace/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestingSpace
+//
+//  Created by Manuel Aurora on 18.07.17.
+//  Copyright © 2017 Manuel Aurora. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
